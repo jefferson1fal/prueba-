13c13d172f1e38a0511acc23dd0765621c1698de
Jefferson Abac <125109849+jefferson1fal@users.noreply.github.com>
2023-04-14 21:17:16 -0600

    Add files via upload

---
+++ LABORATORIO CLASE 8.cs
@@ -0,0 +1,51 @@
+﻿void arreglos()
+{
+    string[] nombre = new string[]
+    {
+        "Fernanda", "Melvin", "Luis", "Brandon", "Yamileth"
+    };
+
+    //foreach (string n in nombre)
+    //{
+    //    Console.WriteLine(n);
+    //}
+
+    int[] notas = new int[]
+    {
+        70, 90, 43, 85, 30
+    };
+
+    //int promedio = 0;
+    //foreach (int i in notas) 
+    //{
+    //    promedio += i;
+    //}
+    //promedio = promedio/notas.Length;
+    //Console.WriteLine($"El Promedio es: {promedio}");
+
+    int nummayor = notas.Max();
+    int indmayor = 0;
+
+    for (int i = 0; i < notas.Length; i++)
+    {
+        if (notas[i] > notas[indmayor])
+        {
+            indmayor = i;
+        }
+    }
+
+    Console.WriteLine(nummayor + " " + nombre[indmayor]);
+
+}
+
+arreglos();
+//    int promedio = 0;
+//    foreach (int i in notas)
+//    {
+//        promedio = promedio + i;
+//    }
+//    promedio = promedio / notas.Length;
+//    Console.WriteLine($"el promedio es :{promedio}");
+//}
+
+
